5d0e1dfcb838f8bee8b0f849ad4d59a442cde7bd
Sascha van Vliet <saschacharlotte@gmail.com>
2025-06-05 01:43:59 +0200

    #3 #4 Alle benodigde dynamische data ingeladen
    Doordat ik eerst casesResponse.data had ingevoerd werkte de dynamische code niet. Dit komt doordat er helemaal geen 'data' in de json-file staat waardoor de query ook niet door data heen kan navigeren.

--- server.js
+++ server.js
@@ -7,19 +7,33 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// Maak werken met data uit formulieren iets prettiger
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
 const engine = new Liquid();
 app.engine('liquid', engine.express());
 app.set('views', './views');
 
-app.get('/', async (req, res) => {
-  res.render('index.liquid')
-})
+const API = 'https://labelvier.nl/wp-json';
+
+const casesResponse = await fetch(API + '/wp/v2/cases')
+const casesResponseJSON = await casesResponse.json();
 
 
+app.get('/', async function (request, response) {
+    response.render('index.liquid', {
+      cases: casesResponseJSON
+  });
+});
+
+app.get('/detail', async function (request, response) {
+    response.render('detail.liquid', {
+  });
+});
 
 // Port live zetten 
 app.set('port', process.env.PORT || 8888);
 app.listen(app.get('port'), () => {
   console.log(`App running on http://localhost:${app.get('port')}`);
 });
-
