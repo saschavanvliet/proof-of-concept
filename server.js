2d8c7c9c5dfe3c5fd312afb8e2af4c3de2a0679d
Sascha van Vliet <saschacharlotte@gmail.com>
2025-06-05 00:31:29 +0200

    Node klaargezet #3 #5
    Echt het beginnetje. Pakketten geinstalleerd met npm install, npm install express en npm install liquid.

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-console.log('Hier komt je server voor Sprint 12.')
+// Belangrijke pakketten installeren
+import express from 'express';
+import { Liquid } from 'liquidjs';
+
+// public & views map aanmaken
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+const engine = new Liquid();
+app.engine('liquid', engine.express());
+app.set('views', './views');
+
+app.get('/', async (req, res) => {
+  res.render('index.liquid')
+})
+
+
+
+// Port live zetten 
+app.set('port', process.env.PORT || 8888);
+app.listen(app.get('port'), () => {
+  console.log(`App running on http://localhost:${app.get('port')}`);
+});
+
