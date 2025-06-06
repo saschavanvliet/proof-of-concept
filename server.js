f27aebd94a4e400379947cff181a179ebdca613a
Sascha van Vliet <saschacharlotte@gmail.com>
2025-06-06 13:40:39 +0200

    Alle data per pagina toegevoegd #4 #3 #9

--- server.js
+++ server.js
@@ -17,19 +17,36 @@ app.set('views', './views');
 
 const API = 'https://labelvier.nl/wp-json';
 
-const casesResponse = await fetch(API + '/wp/v2/cases')
-const casesResponseJSON = await casesResponse.json();
+const cases1Response = await fetch(API + '/wp/v2/cases?per_page=8')
+const cases1ResponseJSON = await cases1Response.json();
 
+const cases2Response = await fetch(API + '/wp/v2/cases?per_page=8&offset=8')
+const cases2ResponseJSON = await cases2Response.json();
+
+const cases3Response = await fetch(API + '/wp/v2/cases?per_page=8&page=3')
+const cases3ResponseJSON = await cases3Response.json();
 
 app.get('/', async function (request, response) {
     response.render('index.liquid', {
-      cases: casesResponseJSON
   });
 });
 
-app.get('/detail', async function (request, response) {
-    response.render('detail.liquid', {
+app.get('/cases', async function (request, response) {
+    response.render('cases.liquid', {
+      cases1: cases1ResponseJSON
   });
+});
+
+app.get('/cases/page2', async function (request, response) {
+    response.render('page2.liquid', {
+        cases2: cases2ResponseJSON
+    });
+});
+
+app.get('/cases/page3', async function (request, response) {
+    response.render('page3.liquid', {
+        cases3: cases3ResponseJSON
+    });
 });
 
 // Port live zetten 
